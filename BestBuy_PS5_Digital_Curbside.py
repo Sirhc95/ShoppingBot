cae384457e643efb2d693e2db1a86576be3a0937
Chris Crump <chris@cypheros.co>
2020-11-22 23:29:37 -0500

    BestBuy_Curbside: Cleanup inputs for dropdown menus
    We can reuse enterData function for these inputs

--- BestBuy_PS5_Digital_Curbside.py
+++ BestBuy_PS5_Digital_Curbside.py
@@ -10,30 +10,30 @@ def order():
     phone = '//*[@id="user.phone"]'
     confirmInfo = '//*[@class="button--continue"]/button'
     creditCardNum = '//*[@id="optimized-cc-card-number"]'
-    creditExpireMonth = "//select[@name='expiration-month']/option[@value='12']" #<- Change the number to your expire month
-    creditExpireYear = "//select[@name='expiration-year']/option[@value='2025']" #<- Change the number to your expire year
+    creditExpireMonth = "//select[@name='expiration-month']"
+    creditExpireYear = "//select[@name='expiration-year']"
     creditCVV = '//*[@id="credit-card-cvv"]'
     firstName ='//*[@id="payment.billingAddress.firstName"]'
     lastName = '//*[@id="payment.billingAddress.lastName"]'
     address = '//*[@id="payment.billingAddress.street"]'
     city = '//*[@id="payment.billingAddress.city"]'
-    state = "//select[@id='payment.billingAddress.state']/option[@value='NY']"
+    state = "//select[@id='payment.billingAddress.state']"
     zipCode = '//*[@id="payment.billingAddress.zipcode"]'
     placeOrder = '//*[@data-track="Place your Order - Contact Card"]'
 
     #KEYS
     #add your information here
-    #NOTE: PLEASE ADD YOUR CREDIT CARD EXPIRE MONTH and YEAR TO VARIABLES ex. @value='12'
     myFirstName = 'John'
     myLastName = 'Smith'
     myEmail = 'mail@gmail.com'
     myPhone = '9803214321'
     myAddress = '1234 Apple Lane'
     myCity = 'New York'
+    myState = 'NY'
     myZipCode = '10025'
     myCreditCardNum = '4232123412341234'
     myCreditExpireMonth = '12'
-    myCreditExpireYear = '25'
+    myCreditExpireYear = '2025'
     myCVV = '123'
 
     #ADDS PS5 TO CART AND GOES TO CHECKOUT
@@ -49,14 +49,14 @@ def order():
 
     #FILLS OUT PAYMENT
     enterData(creditCardNum, myCreditCardNum)
-    clickButton(creditExpireMonth)
-    clickButton(creditExpireYear)
+    enterData(creditExpireMonth, myCreditExpireMonth)
+    enterData(creditExpireYear, myCreditExpireYear)
     enterData(creditCVV, myCVV)
     enterData(firstName, myFirstName)
     enterData(lastName, myLastName)
     enterData(address, myAddress)
     enterData(city, myCity)
-    clickButton(state)
+    enterData(state, myState)
     enterData(zipCode, myZipCode)
 
 
@@ -81,6 +81,6 @@ def enterData(field,data):
 
 if __name__ == "__main__":
     driver = webdriver.Chrome("C:/Users/chris/Downloads/chromedriver_win32/chromedriver.exe")
-    driver.get('https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161')
+    driver.get('https://www.bestbuy.com/site/sony-playstation-5-dualsense-wireless-controller/6430163.p?skuId=6430163')
     time.sleep(3)
     order()
